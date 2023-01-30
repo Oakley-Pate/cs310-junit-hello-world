64e6e9b85ea2f8bd89bddd447ba399c2d6d30a72
Oakley Pate <opate@stu.jsu.edu>
2023-01-29 21:48:29 -0600

    Completed Participation Assignment 2

--- src/edu/jsu/mcis/cs310/Main.java
+++ src/edu/jsu/mcis/cs310/Main.java
@@ -17,7 +17,9 @@ public class Main {
     }
     
     public String reverse(String message) {
-        return message;
+        StringBuilder ogStr = new StringBuilder(message);
+        StringBuilder newStr = ogStr.reverse();
+        return newStr.toString();
     }
     
 }
